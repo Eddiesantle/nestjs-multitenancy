e176ccafa5bab66ea4108c8b7cadfe26faa7f2da
Eddiesantle <wedleys8@gmail.com>
2024-06-24 22:08:08 -0300

    FEAT: integration between systems

--- apps/partner2/src/main.ts
+++ apps/partner2/src/main.ts
@@ -1,15 +1,8 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Partner2Module } from './partner2.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(Partner2Module);
-  const port = 3001;
-
-  await app.listen(port, () => {
-    const address = app.getHttpServer().address();
-    const host = address.address === '::' ? 'localhost' : address.address; // Handling IPv6 addresses
-    Logger.log(`Application is running on: http://${host}:${port}`, 'Bootstrap');
-  });
+  await app.listen(3001);
 }
 bootstrap();
--- apps/partner2/src/partner2.module.ts
+++ apps/partner2/src/partner2.module.ts
@@ -1,11 +1,21 @@
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { Partner2Controller } from './partner2.controller';
-import { Partner2Service } from './partner2.service';
+
+import { AuthCoreModule, PrismaCoreModule } from '@app/core';
+import { EventosModule } from './eventos/eventos.module';
+import { LugaresModule } from './lugares/lugares.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env.partner2', isGlobal: true })],
-  controllers: [Partner2Controller],
-  providers: [Partner2Service],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.partner2',
+      isGlobal: true,
+    }),
+    AuthCoreModule,
+    PrismaCoreModule,
+    EventosModule,
+    LugaresModule,
+  ],
 })
 export class Partner2Module { }
